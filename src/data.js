12bb2ce0dde6b712e612ebb6cb7b748c4bfb1633
Kapileswar Rout <kapileswarrout143@gmail.com>
2023-07-29 00:12:55 +0530

    commit complete

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const companiesData = [
+    {
+      type: 'food industry',
+      problem: 'Food Safety & Quality',
+      description: 'Contamination, spoilage, and mishandling challenge maintaining high standards, leading to illnesses and recalls',
+    },
+    {
+      type: 'food industry',
+      problem: 'Supply Chain Disruptions',
+      description: 'Complex supply chains are vulnerable to natural disasters, geopolitical events, transportation issues, or health crises, causing scarcity and price fluctuations.',
+    },
+    {
+      type: 'food industry',
+      problem: 'Sustainability & Environment',
+      description: 'Reducing food waste, eco-friendly packaging, and responsible sourcing address environmental concerns',
+    },
+    {
+      type: 'food industry',
+      problem: 'Changing Consumer Preferences',
+      description: 'Evolving tastes driven by health trends require innovative products to meet demand.',
+    },
+    {
+      type: 'food industry',
+      problem: 'Labor Shortages & Skills Gap',
+      description: 'Attracting skilled workers, especially in food science and sustainability, is challenging for the industry.',
+    },
+    
+  ];
